0107a09fb56b2f4851e561275191283688dd6532
Якимцев Даниил Владимирович <YAkimtsev@kzgroup.local>
2022-10-31 16:25:34 +0300

    Добавьте файлы проекта.

---
+++ WpfDBViewer/ApplicationVM.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDBViewer
+{
+    class ApplicationVM : INotifyPropertyChanged
+    {
+
+        private Phone selectedPhone;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<Phone> Phones { get; set; }
+
+        public Phone SelectedPhone
+        {
+            get { return selectedPhone; }
+            set
+            {
+                selectedPhone = value;
+                OnPropertyChanged("SelectedPhone");
+            }
+        }
+
+        public ApplicationVM()
+        {
+            Phones = new ObservableCollection<Phone>
+            {
+                new Phone {Name="iPhone 7", Company="Apple", Price=56000 },
+                new Phone {Name="Galaxy S7 Edge", Company="Samsung", Price =60000 },
+                new Phone {Name="Elite x3", Company="HP", Price=56000 },
+                new Phone {Name="Mi5S", Company="Xiaomi", Price=35000 }
+            };
+        }
+
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
---
+++ WpfDBViewer/Phone.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfDBViewer
+{
+    class Phone : INotifyPropertyChanged
+    {
+
+        private string name;
+        private string company;
+        private int price;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public string Company
+        {
+            get { return company; }
+            set
+            {
+                company = value;
+                OnPropertyChanged("Company");
+            }
+        }
+
+        public int Price
+        {
+            get { return price; }
+            set
+            {
+                price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+            }
+        }
+    }
+}
